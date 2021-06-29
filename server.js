6cffe0792c322dfc4d740e80c7b371270afd7051
Trenton <markhamtrenton@gmail.com>
2021-06-29 18:07:28 -0400

    added models and gitignore

--- server.js
+++ server.js
@@ -2,4 +2,31 @@ const mongoose = require("mongoose");
 const express = require("express");
 const logger = require("morgan");
 
+
+const app = express();
+
+//use logger
+app.use(logger("dev"));
+
+//parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//use static files
+app.use(express.static("public"));
+
+
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/WorkoutTracker", { useNewUrlParser: true });
+
 const PORT = process.env.PORT || 3000;
+
+//require('./seeders/seed')
+
+//use routes
+require('./routes/api-routes')(app)
+require('./routes/html-routes')(app)
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}..`);
+})
