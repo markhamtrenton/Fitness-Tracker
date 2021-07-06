765db3dd6a93fd4ce6a3cfc2235080274238e1ec
Trenton <markhamtrenton@gmail.com>
2021-07-06 14:20:03 -0400

    added scout

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const scout = require("@scout_apm/scout-apm");
 const mongoose = require("mongoose");
 const express = require("express");
 const logger = require("morgan");
+
+
+
+
+
 
 
 
@@ -53,3 +59,30 @@ require('./routes/html-routes')(app)
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}..`);
 })
+
+
+async function start() {
+    // Trigger the download and installation of the core-agent
+    await scout.install({
+      allowShutdown: true, // allow shutting down spawned scout-agent processes from this program
+      monitor: true, // enable monitoring
+      name: "",
+      key: "4TYfmML9MUcMMh7Root0",
+    });
+  
+    // Initialize your express application
+    const app = express();
+  
+    // Enable the app-wide scout middleware
+    app.use(scout.expressMiddleware());
+  
+    // Add other middleware and routes
+    // app.use( ... )
+    // app.get( ... )
+  
+    // Start express
+    app.start();
+  }
+  
+  // If this script is executed directly, run the start function
+  if (require.main === module) { start(); } 
